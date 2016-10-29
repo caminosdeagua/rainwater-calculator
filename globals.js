834a8853fad1a987c5fc641051bcf7db2ca355b7
ask53 <askrupp@gmail.com>
2016-10-29 13:19:20 -0500

    bunch more validation updates

---
+++ globals.js
@@ -0,0 +1,20 @@
+var form;
+
+var allData;
+
+
+var MAX_PPL = 9999;
+var MIN_PPL = 1;
+
+
+
+
+
+var numAcceptableKeys = ["ArrowLeft", "ArrowRight", 		// Define which keys should be functional
+							"Backspace", "Delete", 
+							"Tab", "ArrowUp", "ArrowDown", 
+							"Home", "End", "Insert", 
+							"PageUp", "PageDown", "Escape",
+							"0", "1", "2", "3", "4", "5", 
+							"6", "7", "8", "9", "."
+];
